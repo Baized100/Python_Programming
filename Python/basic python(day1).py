1b835c2f0f1e8b3017a234a8f2b9091dfdbf910a
Md.Baizid <111190288+Baized100@users.noreply.github.com>
2024-10-31 18:56:28 +0600

    Add files via upload

---
+++ Python/basic python(day1).py
@@ -0,0 +1,23 @@
+#(input,output,variable) 
+print("hlw baizid",end="!!")
+name="MOhammad shake"
+print(name)
+print("my son name is" + name)
+x,y,z=10,20,"baizid"
+print(x)
+print(z)
+varsity_name="Barisal information techonology"#snack case
+#local variable,global variable
+
+x=10 #global
+def func2():
+    x=33 #local
+    print("my roll number is:",x)#local
+func2()
+print(x)#global print
+
+cars=100
+drivers=30
+cars_not_driven=cars-drivers
+print(cars_not_driven)
+
